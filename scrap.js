b916c49ab8550e3c65ef783925ee55f2a3428e50
h15200 <45977354+h15200@users.noreply.github.com>
2020-07-25 10:04:29 -0700

    add scrap code

--- scrap.js
+++ scrap.js
@@ -8,3 +8,23 @@
 
 // const a = new Set([1, 1, 1, 2, 3]);
 // console.log(a);
+class Test {
+  constructor(name) {
+    this.name = name;
+  }
+  greet() {
+    console.log('hi');
+  }
+}
+const test = new Test('h');
+console.log(test);
+
+const Test2 = function (name) {
+  this.name = name;
+};
+Test2.prototype.greet = function () {
+  console.log('hi');
+};
+
+const test2 = new Test2('p');
+test2.greet();
