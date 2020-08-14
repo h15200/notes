a375c378751b74b7f9d503b386df6756d4b90494
h15200 <45977354+h15200@users.noreply.github.com>
2020-08-14 16:50:16 -0700

    threeNumberSum

---
+++ algos/threeNumberSum.js
@@ -0,0 +1,26 @@
+// return an ordered array of arrays.
+
+function threeNumberSum(array, targetSum) {
+  // combination of sliding window and pointers O(n^2)
+  // immeidately sort since O(n^2) is already worse than O(nlogn)
+  const output = [];
+  array = array.sort((a, b) => a - b);
+  // store i - 1, start for loop on i, go up to length - 1 (can't make triplet without 3 ele)
+
+  for (let i = 1; i < array.length - 1; i++) {
+    let firstNum = array[i - 1];
+    let left = i;
+    let right = array.length - 1;
+    while (left < right) {
+      const total = firstNum + array[left] + array[right];
+      if (total === targetSum) {
+        output.push([firstNum, array[left], array[right]]);
+        left++;
+      } else if (total < targetSum) left++;
+      else right--;
+    }
+  }
+  return output;
+}
+
+console.log(threeNumberSum([12, 3, 1, 2, -6, 5, 0, -8, -1, 6], 0));
