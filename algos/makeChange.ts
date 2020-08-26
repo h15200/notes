55c486cd54a9a955b63c33e3e778ce7658e95c52
h15200 <45977354+h15200@users.noreply.github.com>
2020-08-26 10:59:01 -0700

    makechange bubble up recursion

--- algos/makeChange.ts
+++ algos/makeChange.ts
@@ -1,2 +1,47 @@
-let a: Number = 4;
-console.log('hi');
+function numberOfWaysToMakeChange(n: number, denoms: number[]): number {
+  // helper function to check to see
+
+  // edge case n doesn't exist or is 0
+  if (n === undefined || n < 0) return -1;
+  if (n === 0) return 1;
+
+  // initialize cache like this {  1: 0, 5: 0, 10: 0 }
+  const cache: Object = {};
+  denoms.forEach((coin) => (cache[String(coin)] = 0));
+
+  // helper function to compare objects
+  const compareCoins = (obj1, obj2) => {
+    for (const key in obj1) if (obj1[key] !== obj2[key]) return false;
+    for (const key in obj2) if (obj2[key] !== obj1[key]) return false;
+    return true;
+  };
+  let ways: number = 0;
+  const recorded: Object[] = [];
+  // n is bigger than 0
+  (function generateCombo(balance: number, coinsUsed: Object = cache): void {
+    // recorded is a list of objects representing existing combos of coins
+    // base case - if balance is 0
+    if (balance === 0) {
+      // compare against recorded
+      if (recorded.every((cached) => !compareCoins(cached, coinsUsed))) {
+        console.log('hit. coins is', coinsUsed);
+        ways++;
+        recorded.push(coinsUsed);
+        return;
+      }
+    }
+
+    for (let coin of denoms) {
+      if (balance >= coin) {
+        generateCombo(balance - coin, {
+          ...coinsUsed,
+          [coin]: coinsUsed[coin] + 1,
+        });
+      }
+    }
+  })(n);
+  console.log(recorded);
+  return ways;
+}
+
+console.log(numberOfWaysToMakeChange(10, [10, 5, 2]));
