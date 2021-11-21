a1a364675d308d642ab300acb66fce5a4539717c
h15200 <45977354+h15200@users.noreply.github.com>
2021-11-20 18:20:56 -0800

    vectors and sort

--- c++/stroustrup_ch4.cpp
+++ c++/stroustrup_ch4.cpp
@@ -42,25 +42,89 @@
 
 //! pg 125
 
+// #include <iostream>
+// #include <vector>
+// #include <string>
+
+// int main() {
+//   std::cout << "Please enter a bunch of strings separated by a space but do NOT use the word poop. Press control-D twice when you're done.\n\n";
+//   std::vector<std::string> words;
+//   for (std::string word; std::cin >> word;) {
+//     words.push_back(word);
+//   }
+//   std::cout << "\n \n You said: \n\n";
+//   for (std::string word : words) {
+//     if (word == "poop") {
+//       std::cout << "beep" << "\n";
+//     } else {
+//       std::cout << word << "\n";
+//     }
+//   }
+    
+//   return 0;
+// }
+
+//! drill
+
 #include <iostream>
-#include <vector>
+#include <algorithm>
+#include <limits>
 #include <string>
+#include <vector>
+
+double ConvertToMeter(double num, std::string unit) {
+  if (unit == "cm") {
+    return num / 100;
+  } else if (unit == "in") {
+    return num * 2.54 / 100;
+  } else if (unit == "ft") {
+    return num * 12 * 2.54 / 100;
+  } else if (unit == "m") {
+    return num;
+  } else {
+    return 0;
+  }
+}
 
 int main() {
-  std::cout << "Please enter a bunch of strings separated by a space but do NOT use the word poop. Press control-D twice when you're done.\n\n";
-  std::vector<std::string> words;
-  for (std::string word; std::cin >> word;) {
-    words.push_back(word);
-  }
-  std::cout << "\n \n You said: \n\n";
-  for (std::string word : words) {
-    if (word == "poop") {
-      std::cout << "beep" << "\n";
-    } else {
-      std::cout << word << "\n";
+  std::cout << "Enter a bunch of floats with units cm, m, in, or ft (ex 23.1 cm) and press \"|\"\n";
+  double min = std::numeric_limits<double>::max();
+  double max = std::numeric_limits<double>::min();
+  double sum_meter_vals = 0;
+  int num_values = 0;
+  std::vector<double> meter_vector{};
+
+  double num;
+  std::string unit;
+  while (std::cin >> num >> unit) {
+    if (unit != "m" && unit != "ft" && unit!= "cm" && unit!= "in") {
+      std::cout << "Illegal unit name\n";
+      return 1;
+    }
+    ++num_values;
+    double meter_value = ConvertToMeter(num, unit);
+    sum_meter_vals += meter_value;
+    meter_vector.push_back(meter_value);
+    if (meter_value < min) {
+      min = meter_value;
+    } if (meter_value > max) {
+      max = meter_value;
     }
   }
-    
+
+  std::cout << "Maximum value entered was " << max << "m\n";
+  std::cout << "Minimum value entered was " << min << "m\n";
+  std::cout << "Number of values entered was " << num_values << "\n";
+  std::cout << "Sum of all values is " << sum_meter_vals << "m\n";
+  
+  std::sort(meter_vector.begin(), meter_vector.end());
+  std::cout << "Listing all values in increasing order: \n"; 
+  for (double val : meter_vector) {
+    std::cout << val << "m\n"; 
+  }
+
   return 0;
 }
+
+
 
