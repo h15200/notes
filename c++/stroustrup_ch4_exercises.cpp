dfe903e48d8d563975452e17d36b43b7bd607520
h15200 <45977354+h15200@users.noreply.github.com>
2021-11-22 18:24:12 -0800

    c++ ch4 ex to 12

--- c++/stroustrup_ch4_exercises.cpp
+++ c++/stroustrup_ch4_exercises.cpp
@@ -46,50 +46,307 @@
 
 //! ex4 number guessing game
 
-#include <iostream>
+// #include <iostream>
 
-int main() {
-  // do binary search with io
-  std::cout << "Think of a number beween 1 and 100\n";
-  int left = 1;
-  int right = 100;
-  while (left < right) {
-    int mid = (left + right) / 2;
-    if (mid == left || mid == right) {
-      break;
-    }
-    if (left == right) {
-      std::cout << "The number must be " << left << "!\n";
-      return 0;
-    }
-    std::cout << "Is your number greater or equal to " << mid << " ? (Enter 'y' or 'n')\n";
-    char reply;
-    std::cin >> reply;
-    switch (reply) {
-      case 'y':
-        left = mid;
-        break;
-      case 'n':
-        right = mid - 1;
-        break;
-      default :
-        std::cout << "That's not a valid answer\n";
-        return 1;
-    }
-  }
-  // one of the two
-  std::cout << "Is your number " << left << "?\n";
-  char outer_reply;
-  std::cin >> outer_reply;
-  switch (outer_reply) {
-    case 'y':
-      std::cout << "Yay!\n";
-      return 0;
-    case 'n':
-      std::cout << "It must be " << right << "!\n";
-      return 0;
-  }
+// int main() {
+//   // do binary search with io
+//   std::cout << "Think of a number beween 1 and 100\n";
+//   int left = 1;
+//   int right = 100;
+//   while (left < right) {
+//     int mid = (left + right) / 2;
+//     if (mid == left || mid == right) {
+//       break;
+//     }
+//     if (left == right) {
+//       std::cout << "The number must be " << left << "!\n";
+//       return 0;
+//     }
+//     std::cout << "Is your number greater or equal to " << mid << " ? (Enter 'y' or 'n')\n";
+//     char reply;
+//     std::cin >> reply;
+//     switch (reply) {
+//       case 'y':
+//         left = mid;
+//         break;
+//       case 'n':
+//         right = mid - 1;
+//         break;
+//       default :
+//         std::cout << "That's not a valid answer\n";
+//         return 1;
+//     }
+//   }
+//   // one of the two
+//   std::cout << "Is your number " << left << "?\n";
+//   char outer_reply;
+//   std::cin >> outer_reply;
+//   switch (outer_reply) {
+//     case 'y':
+//       std::cout << "Yay!\n";
+//       return 0;
+//     case 'n':
+//       std::cout << "It must be " << right << "!\n";
+//       return 0;
+//   }
+// }
+
+//! ex 5 which be becamse 7
+
+// #include <iostream>
+// #include<string>
+
+// int main() {
+//   std::cout << "Type in 2 numbers and an operation (+, -, x or /)\n";
+//   std::string digit_1;
+//   std::string digit_2;
+//   char operation;
+//   std::cin >> digit_1 >> digit_2 >> operation;
+//   std::string operation_string;
+//   int num_1;
+//   int num_2;
+//   if (digit_1 == "0" || digit_1 == "zero") {
+//     num_1 = 0;
+//   } 
+//   if (digit_2 == "0" || digit_2 == "zero") {
+//     num_2 = 0;
+//   }
+//   if (digit_1 == "1" || digit_1 == "one") {
+//     num_1 = 1;
+//   } 
+//   if (digit_2 == "1" || digit_2 == "one") {
+//     num_2 = 1;
+//   }if (digit_1 == "2" || digit_1 == "two") {
+//     num_1 = 2;
+//   } 
+//   if (digit_2 == "2" || digit_2 == "two") {
+//     num_2 = 2;
+//   }if (digit_1 == "3" || digit_1 == "three") {
+//     num_1 = 3;
+//   } 
+//   if (digit_2 == "3" || digit_2 == "three") {
+//     num_2 = 3;
+//   }if (digit_1 == "4" || digit_1 == "four") {
+//     num_1 = 4;
+//   } 
+//   if (digit_2 == "4" || digit_2 == "four") {
+//     num_2 = 4;
+//   }if (digit_1 == "5" || digit_1 == "five") {
+//     num_1 = 5;
+//   } 
+//   if (digit_2 == "5" || digit_2 == "five") {
+//     num_2 = 5;
+//   }if (digit_1 == "6" || digit_1 == "six") {
+//     num_1 = 6;
+//   } 
+//   if (digit_2 == "6" || digit_2 == "six") {
+//     num_2 = 6;
+//   }if (digit_1 == "7" || digit_1 == "seven") {
+//     num_1 = 7;
+//   } 
+//   if (digit_2 == "7" || digit_2 == "seven") {
+//     num_2 = 7;
+//   }if (digit_1 == "8" || digit_1 == "eight") {
+//     num_1 = 8;
+//   } 
+//   if (digit_2 == "8" || digit_2 == "eight") {
+//     num_2 = 8;
+//   }if (digit_1 == "9" || digit_1 == "nine") {
+//     num_1 = 9;
+//   } 
+//   if (digit_2 == "9" || digit_2 == "nine") {
+//     num_2 = 9;
+//   }
 
 
- 
-}
+//   double solution;
+//   switch (operation) {
+//     case '+':
+//       operation_string = "sum";
+//       solution = num_1 + num_2;
+//       break;
+//     case '-':
+//       operation_string = "difference";
+//       solution = num_1 - num_2;
+//       break;
+//     case 'x':
+//       operation_string = "product";
+//       solution = num_1 * num_2;
+//       break;
+//     case '/':
+//       operation_string = "quotient";
+//       solution = num_1 / num_2;
+//       break;
+//     default:
+//       std::cout << "Invalid Operation\n";
+//       return 1;
+//   }
+//   std::cout << "The " << operation_string << " of " << num_1 << " and " << num_2 << " is " << solution << "!\n";
+//   return 0;
+// }
+
+//! ex 6
+
+// #include <iostream>
+// #include <string>
+// #include <vector>
+
+// int main () {
+//   std::vector<std::string>str_v{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+//   std::cout << "Enter a number between 0 and 9\n";
+//   std::string user_string;
+//   std::cin >> user_string;
+//   if (user_string == "zero" || user_string == "0") {
+//     std::cout << str_v[0] << "\n";
+//   } else if (user_string == "one" || user_string == "1") {
+//     std::cout << str_v[1] << "\n";
+//   } else if (user_string == "two" || user_string == "2") {
+//     std::cout << str_v[2] << "\n";
+//   } else if (user_string == "three" || user_string == "3") {
+//     std::cout << str_v[3] << "\n";
+//   } else if (user_string == "four" || user_string == "4") {
+//     std::cout << str_v[4] << "\n";
+//   } else if (user_string == "five" || user_string == "5") {
+//     std::cout << str_v[5] << "\n";
+//   } else if (user_string == "six" || user_string == "6") {
+//     std::cout << str_v[6] << "\n";
+//   } else if (user_string == "seven" || user_string == "7") {
+//     std::cout << str_v[7] << "\n";
+//   } else if (user_string == "eight" || user_string == "8") {
+//     std::cout << str_v[8] << "\n";
+//   } else if (user_string == "nine" || user_string == "9") {
+//     std::cout << str_v[9] << "\n";
+//   } else {
+//     std::cout << "Invalid\n";
+//     return 1;
+//   }
+//   return 0;
+// }
+
+//! ex 8
+
+// #include <iostream>
+
+// int CalculateNumberOfDoubles(int total) {
+//   int i = 1;
+//   int curr = 1;
+//   while (true) {
+//     std::cout << "Current total is " << curr << " current iteration is " << i << "\n"; 
+//     if (curr >= total) {
+//       return i;
+//     }
+//     curr *= 2;
+//     ++i;
+//   }
+// }
+
+// int main() {
+//   std::cout << CalculateNumberOfDoubles(1000) << "\n";
+//   std::cout << CalculateNumberOfDoubles(1000000) << "\n";
+//   std::cout << CalculateNumberOfDoubles(1000000000) << "\n";
+//   return 0;
+// }
+
+//! ex 9
+
+// #include <iostream>
+
+// int CalculateDoubles(int iterations) {
+//   int i = 1;
+//   // int curr = 1;
+//   double curr = 1.0;
+//   while (i <= iterations) {
+//     std::cout << "Current total is " << curr << " current iteration is " << i << "\n"; 
+//     curr *= 2;
+//     ++i;
+//   }
+//   return curr;
+// }
+
+// int main() {
+//   std::cout << CalculateDoubles(64) << "\n";
+//   return 0;
+// }
+
+//! ex 10
+
+// #include <iostream>
+// #include <vector>
+
+// int main() {
+//   std::cout << "Rock, Paper, Scissors!\n";
+//   std::cout << "Enter a number between 1 and 100\n";
+//   int computer_int;
+//   std::cin >> computer_int;
+//   int computer_hand = computer_int % 3;
+
+//   std::cout << "Enter 0 for Rock, 1 for Paper, and 2 for Scissors\n";
+//   int user_hand;
+//   std::cin >> user_hand;
+//   std::vector<std::string>hand_v{"Rock", "Paper", "Scissors"};
+  
+//   if (user_hand == computer_hand) {
+//     std::cout << "The computer chose " << hand_v[computer_hand] << " too! You Tied!\n";
+//   }
+//   else if (user_hand == 0) {
+//     if (computer_hand == 1) {
+//       std::cout << "The computer chose " << hand_v[computer_hand] << "! You Lost!\n";
+//     } else {
+//       std::cout << "The computer chose " << hand_v[computer_hand] << "! You Won!\n";
+//     }
+//   } else if (user_hand == 1) {
+//     if (computer_hand == 2) {
+//       std::cout << "The computer chose " << hand_v[computer_hand] << "! You Lost!\n";
+//     } else {
+//       std::cout << "The computer chose " << hand_v[computer_hand] << "! You Won!\n";
+//     }
+//   } else if (user_hand == 2){
+//     if (computer_hand == 0) {
+//       std::cout << "The computer chose " << hand_v[computer_hand] << "! You Lost!\n";
+//     } else {
+//       std::cout << "The computer chose " << hand_v[computer_hand] << "! You Won!\n";
+//     }
+//   } else {
+//     std::cout << "Invalid user number \n";
+//   }
+//   return 0;
+// }
+
+//! ex 11, then 12
+
+// all primes between 1 and 100
+// first prime is 2
+
+// #include <vector>
+// #include <iostream>
+
+// std::vector<int> FindPrimesTo(int num) {
+//   std::vector<int>primes_v{};
+//   if (num < 2) return primes_v;
+//   primes_v.push_back(2);
+//   int i = 3;
+//   while (i <= num) {
+//     bool isPrime = true;
+//     for (int prime: primes_v) {
+//       if (i % prime == 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     if (isPrime == true) {
+//       primes_v.push_back(i);
+//     }
+//     i += 2; 
+//   }
+  
+//   return primes_v;
+// }
+
+// int main() {
+//   std::vector<int>primes_v = FindPrimesTo(100);
+//   std::cout << "Printing all primes: ]n";
+//   for (int prime : primes_v) {
+//     std::cout << prime << "\n";
+//   }
+//   return 0;
+// }
