8bba9d3549b4adb8ec97636ea8139bf177576e5e
h15200 <45977354+h15200@users.noreply.github.com>
2023-08-20 12:40:12 -0700

    chore: clearn up, make new repo for go

--- python/scratch.py
+++ python/scratch.py
@@ -1,1 +1,10 @@
+from typing import List
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # start top right
+        # if bigger than target, go left
+        # if smaller than taget, go down
+        # if out of bounds, return false
 
+
+        
